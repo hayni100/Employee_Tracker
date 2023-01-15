53ddf163d28e18c524a987e2111947ef471d6ed6
Haein Kim <hayni100@gmail.com>
2023-01-15 14:44:13 -0800

    added code for adddepartment, code revision needed for addrole

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,26 @@
-INSERT INTO department (id, name)
-VALUES (1, "Sales"),
-       (2, "Engineering"),
-       (3, "Finace"),
-       (4, "Legal");
+INSERT INTO department (department_name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finace"),
+       ("Legal");
 
 
-INSERT INTO role (id, title, department, salary)
-VALUE   (1, "Sales Leader", 1, 100000),
-        (2, "Salesperson", 1, 80000),
-        (3, "Lead Engineer", 2, 150000),
-        (4, "Software Engineer", 2, 12000),
-        (5, "Account Manager", 3, 160000),
-        (6, "Accountant", 3, 125000),
-        (7, "Legal Team Lead",4, 250000),
-        (8, "Lawyer", 4, 190000);
+INSERT INTO role (title, department_id, salary)
+VALUE   ("Sales Leader", 1, 100000),
+        ("Salesperson", 1, 80000),
+        ("Lead Engineer", 2, 150000),
+        ("Software Engineer", 2, 12000),
+        ("Account Manager", 3, 160000),
+        ("Accountant", 3, 125000),
+        ("Legal Team Lead",4, 250000),
+        ("Lawyer", 4, 190000);
 
-INSERT INTO employee (id, first_name, last_name, title, department, salary, manager)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE   ("John", "Doe", 1, NULL),
+        ("Jane", "Doe", 3, NULL),
+        ("Carl", "Ung", 7, NULL),
+        ("Kimmie", "Popper", 2, 1),
+        ("yi-san", "Liu", 6, 5),
+        ("Sunhee", "Kim", 4, 3);
+
+     
--- index.js
+++ index.js
@@ -41,10 +41,36 @@ const mainPrompt = [
   },
 ];
 
+// inquirer
+// .prompt ([
+//   {
+//     type: "input",
+//     message: "What is the name of the department?",
+//     name: "department_name",
+//   },
+// ]);
+
+// const addRoleP = [
+//   {
+//     type: "input",
+//     message: "What is the name of the role?",
+//     name: "role_name",
+//   },
+//   {
+//     type: "input",
+//     message: "What is the salary of the role?",
+//     name: "role_salary",
+//   },
+//   {
+//     type: "input",
+//     message: "Which department does the role belong to?",
+//     choices:
+//   }
+// ]
+
 //running tasks from user selection//
 function main() {
   inquirer.prompt(mainPrompt).then((answers) => {
-    console.log(answers);
     if (answers.mainPrompt === "view_departments") {
       viewDepartments();
     }
@@ -68,32 +94,104 @@ function main() {
       updateEmployeeRole();
     }
   });
-  //Choose to view all departments
-  //formatted table showing department names and department ids
-  function viewDepartments() {
- 
-    connection.query(`select * from department `, function (err, results) {
-      console.table(results);
+}
+//Choose to view all departments
+//formatted table showing department names and department ids
+function viewDepartments() {
+  connection.query(`select * from department `, function (err, results) {
+    console.table(results);
+    main();
+  });
+}
+//Choose to view all roles
+//table of job title, role id, the department that role belongs to, and the salary for that role
+function viewRoles() {
+  connection.query(`select * from role`, function (err, results) {
+    console.table(results);
+    main();
+  });
+}
+//Choose to view all employees
+//table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+function viewEmployees() {
+  connection.query(`select * from employee`, function (err, results) {
+    console.table(results);
+    main();
+  });
+}
+//Choose to add a department
+//prompted to enter the name of the department and that department is added to the database
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the department?",
+        name: "department_name",
+      },
+    ])
+    .then((answers) => {
+      console.log(
+        "[ Successfully added " + answers.department_name + " to the database ]"
+      );
+      connection.query("INSERT INTO department SET?", {
+        department_name: answers.department_name,
+      });
       main();
     });
-    //Choose to view all roles
-    //table of job title, role id, the department that role belongs to, and the salary for that role
-
-    //Choose to view all employees
-    //table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
-    //Choose to add a department
-    //prompted to enter the name of the department and that department is added to the database
-
-    //Choose to add a role
-    //prompted to enter the name, salary, and department for the role and that role is added to the database
-
-    //Choose to add an employee
-    //prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-
-    //Choose to update an employee role
-    //prompted to select an employee to update and their new role and this information is updated in the database //
-  }
-
 }
+//Choose to add a role
+//prompted to enter the name, salary, and department for the role and that role is added to the database
+function addRole() {
+  // check all depts
+  connection
+    .promise()
+    .query("SELECT * FROM department")
+    .then((res) => {
+      // create an array based on user input
+      return res[0].map((dept) => {
+        return {
+          name: dept.id,
+          value: dept.name,
+        };
+      });
+    })
+    .then((departments) => {
+      return inquirer.prompt([
+        {
+          type: "input",
+          name: "roles",
+          message: "What is the name of the role?",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary of the role?",
+        },
+        {
+          type: "list",
+          name: "depts",
+          //Only spits out id not names NEED TO FIX also keeps repeating//
+          choices: departments, 
+          message: "Which department does this belong to?",
+        },
+      ]);
+    })
+    .then((answer) => {
+      console.log(answer);
+      return connection.promise().query("INSERT INTO role SET ?", {
+        title: answer.roles,
+        salary: answer.salary,
+        department_id: answer.depts,
+      });
+    });
+}
+//Choose to add an employee
+//prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+function addEmployee() {
+  console.log("You chose to add an employee");
+}
+//Choose to update an employee role
+//prompted to select an employee to update and their new role and this information is updated in the database //
 main();
